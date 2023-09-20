7d5260b30bb513a117b9102b8514d66f29e0c702
Paul Conway <paulc00@gmail.com>
2023-09-19 23:20:57 -0400

    css tidy up

---
+++ Dev Notes.md
@@ -0,0 +1,54 @@
+# Notes
+
+Switching between horizontal flex and vertical flex based on screen size
+<br />
+
+**CSS:**
+```css
+.container {
+  display:flex;
+  flex-direction:column;
+}
+
+.row-container {
+  display:flex;
+  flex-direction:row;
+  justify-content:space-between;
+}
+
+.box {
+  border:3px solid #000;
+  width:300px;
+  height:300px;
+  margin-bottom:10px;
+}
+
+.box2 {
+  width:200px;
+  height:200px;
+ }
+
+@media only screen and (max-width: 600px) {
+  .row-container {
+    flex-direction:column;
+  }
+  
+  .box2 {
+    width:300px;
+    height:300px;
+  }
+}
+```
+
+<br />
+
+**HTML:**
+```html
+<div class="container">
+  <div class="row-container">
+    <div class="box1 box">1</div>
+    <div class="box2 box">2</div>
+  </div>
+  <div class="box3 box">3</div>
+</div>
+```
--- src/App.svelte
+++ src/App.svelte
@@ -4,118 +4,142 @@
 </script>
 
 <main>
-  <!-- <div class="btn-group">
-    <button type="button" class="btn btn__primary">B1</button>
-    <button type="button" class="btn btn__primary">B2</button>
-    <button type="button" class="btn btn__primary">B3</button>
-  </div> -->
-  <ul class="wins">
-    <li class="win">
+  <div class="wins">
+    <div class="win">
       <button class="win-btn win-title">Window 1</button>
-      <ul class="tabs">
-        <li class="tab">
-          <button class="tabs-btn tab-item">
-            <img class="tab-icon" src="/googleg_standard_color_128dp.png" alt="icon" />
+      <div class="tabs">
+        <div class="tab">
+          <button class="tab-btn tab-item">
+            <img
+              class="tab-icon sq-btn"
+              src="/googleg_standard_color_128dp.png"
+              alt="icon"
+            />
             <div class="tab-info">
               <span class="tab-title">Google</span>
               <span class="tab-url">https://www.google.com/</span>
             </div>
           </button>
-          <button class="tabs-btn tab-go-btn">→</button>
-          <button class="tabs-btn tab-susp-btn">Z<sup>Z</sup></button>
-          <button class="tabs-btn tab-close-btn">×</button>
-        </li>
-        <li class="tab">
-          <button class="tabs-btn tab-item">
-            <img class="tab-icon" src="/favicon-trans-bg-blue-mg-png.png" alt="icon" />
+          <button class="tab-btn sq-btn tab-go-btn">→</button>
+          <button class="tab-btn sq-btn tab-susp-btn">Z<sup>Z</sup></button>
+          <button class="tab-btn sq-btn tab-close-btn">×</button>
+        </div>
+        <div class="tab">
+          <button class="tab-btn tab-item">
+            <img
+              class="tab-icon sq-btn"
+              src="/favicon-trans-bg-blue-mg-png.png"
+              alt="icon"
+            />
             <div class="tab-info">
               <span class="tab-title">Bing</span>
               <span class="tab-url">https://www.bing.com/</span>
             </div>
           </button>
-          <button class="tabs-btn tab-go-btn">→</button>
-          <button class="tabs-btn tab-susp-btn">Z<sup>Z</sup></button>
-          <button class="tabs-btn tab-close-btn">×</button>
-        </li>
-      </ul>
-    </li>
-    <li class="win">
+          <button class="tab-btn sq-btn tab-go-btn">→</button>
+          <button class="tab-btn sq-btn tab-susp-btn">Z<sup>Z</sup></button>
+          <button class="tab-btn sq-btn tab-close-btn">×</button>
+        </div>
+      </div>
+    </div>
+    <div class="win">
       <button class="win-btn win-title">Window 2</button>
-      <ul class="tabs">
-        <li class="tab">
-          <button class="tabs-btn tab-item">
-            <img class="tab-icon" src="/DDG-icon_256x256.png" alt="icon" />
+      <div class="tabs">
+        <div class="tab">
+          <button class="tab-btn tab-item">
+            <img class="tab-icon sq-btn" src="/DDG-icon_256x256.png" alt="icon" />
             <div class="tab-info">
               <span class="tab-title">DuckDuckGo — Privacy, simplified.</span>
               <span class="tab-url">https://www.duckduckgo.com/</span>
             </div>
           </button>
-          <button class="tabs-btn tab-go-btn">→</button>
-          <button class="tabs-btn tab-susp-btn">Z<sup>Z</sup></button>
-          <button class="tabs-btn tab-close-btn">×</button>
-        </li>
-        <li class="tab">
-          <button class="tabs-btn tab-item">
-            <img class="tab-icon" src="/hn_favicon.ico" alt="icon" />
+          <button class="tab-btn sq-btn tab-go-btn">→</button>
+          <button class="tab-btn sq-btn tab-susp-btn">Z<sup>Z</sup></button>
+          <button class="tab-btn sq-btn tab-close-btn">×</button>
+        </div>
+        <div class="tab">
+          <button class="tab-btn tab-item">
+            <img class="tab-icon sq-btn" src="/hn_favicon.ico" alt="icon" />
             <div class="tab-info">
               <span class="tab-title">Hacker News</span>
               <span class="tab-url">https://news.ycombinator.com/</span>
             </div>
           </button>
-          <button class="tabs-btn tab-go-btn">→</button>
-          <button class="tabs-btn tab-susp-btn">Z<sup>Z</sup></button>
-          <button class="tabs-btn tab-close-btn">×</button>
-        </li>
-      </ul>
-    </li>
-  </ul>
+          <button class="tab-btn sq-btn tab-go-btn">→</button>
+          <button class="tab-btn sq-btn tab-susp-btn">Z<sup>Z</sup></button>
+          <button class="tab-btn sq-btn tab-close-btn">×</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </main>
 
 <style>
-  .wins, .tabs {
-    list-style-type: none;
+  .wins {
+     display: flex;
   }
 
   .win {
     text-align: left;
-     }
-
-  .tab {
-		display: flex;
-    text-align: left;
+    padding: 2em;
+    width: 32em;
+    display:grid;
   }
 
-  .tabs-btn, .win-btn {
+  .win-title {
+    width:100%;
+  }
+
+  .tab {
+    display: flex;
+    text-align: left;
+    border: 1px solid;
+    width:32rem;
+  }
+
+  .sq-btn {
+    width: 4em;
+    /* height: 4em; */
+  }
+
+  .tab-btn,
+  .win-btn {
     border-radius: 0px;
+    height: 4rem;
   }
 
   .win-btn {
-    margin-top: 0.5em;
+    border: 1px solid;
+    /* margin-top: 0.5em; */
+    /* height: 100%;   */
+    /* width: 32rem; */
+  /* height: 3em; */
   }
 
   .tab-item {
-		display: flex;
-		flex-flow: row nowrap;
-		align-items: center;
-		width: 100%;
-		min-height: 3rem;
-		padding: 0 1rem;
-		overflow: hidden;
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    width: 20em;
+    /* min-height: 3rem; */
+    /* padding: 0 1rem; */
+    
+    overflow: hidden;
   }
 
   .tab-icon {
-		flex: 0 0 1rem;
-		height: 1rem;
-		margin-right: 1rem;
+    flex: 0 0 1rem;
+    height: 1rem;
+    margin-right: 1rem;
   }
 
   .tab-info {
-		display: flex;
-    text-align:left; /* added PC */
-		flex-flow: column nowrap;
-		flex: 1;
-		min-width: 0;
-	}
+    display: flex;
+    text-align: left; /* added PC */
+    flex-flow: column nowrap;
+    flex: 1;
+    min-width: 0;
+  }
 
   /* .tab-box {
   }
@@ -123,14 +147,15 @@
   .tab-entry {
   } */
 
-  .tab-title, .tab-url {
+  .tab-title,
+  .tab-url {
     overflow: hidden;
-		text-overflow: ellipsis;
-		white-space: nowrap;
+    text-overflow: ellipsis;
+    white-space: nowrap;
   }
 
   .tab-url {
-		font-size: 80%;
-		opacity: 0.5;
+    font-size: 80%;
+    opacity: 0.5;
   }
 </style>
